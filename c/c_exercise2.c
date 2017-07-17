102c1f1a65eab52a60b07f5a63fd9b74ba5d42f1
Rob Underwood <runderwood5@gmail.com>
2017-07-17 19:10:42 -0400

    Adding exercise 2 C file

---
+++ c/c_exercise2.c
@@ -0,0 +1,19 @@
+/* C - Exercise 2 */
+
+/* Based on https://www.hackerrank.com/challenges/fp-solve-me-first
+- "Solve Me First FP" */
+
+#include <stdio.h>
+
+int main() {
+	int a;
+	int b;
+	int result;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
+
+	result = a + b;
+
+	printf("%d\n", result);
+}
