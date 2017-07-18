1f95ab3a9e0eda73b84436d3be67fcc2b9aacbba
Rob Underwood <runderwood5@gmail.com>
2017-07-17 23:20:37 -0400

    Exercise 1 in java

---
+++ java/Java_Exercise_1.java
@@ -0,0 +1,18 @@
+/*
+	Based on http://introcs.cs.princeton.edu/java/11hello/HelloWorld.java.html
+*/
+
+/*
+		Compile at command line with
+		javac Java_Exercise_1.java
+		then run with
+		java Java_Exercise_1
+		Also can do it w/ a single command such as
+		javac Java_Exercise_1.java && java Java_Exercise_1
+*/
+
+public class Java_Exercise_1 {
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+    }
+}
