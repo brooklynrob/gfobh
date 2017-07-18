4c7dce0ced34f5d90b615d62b908175856f77399
Rob Underwood <runderwood5@gmail.com>
2017-07-17 22:57:06 -0400

    Exercise 1 in C#

---
+++ csharp/Csharp_Exercise_1.cs
@@ -0,0 +1,10 @@
+// Source: https://msdn.microsoft.com/en-us/library/aa288463(v=vs.71).aspx
+// csharp_exercise_1.cs
+
+public class Csharp_Exercise_1
+{
+ public static void Main()
+ {
+		System.Console.WriteLine("Hello, World!");
+ }
+}
