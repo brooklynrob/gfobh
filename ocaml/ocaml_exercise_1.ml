cd1cad681ca8458dc0e7f471c9975b20775c3e58
Rob Underwood <runderwood5@gmail.com>
2017-07-17 22:59:24 -0400

    OCaml Exercise 1

---
+++ ocaml/ocaml_exercise_1.ml
@@ -0,0 +1,11 @@
+(* OCaml - Exercise 1 *)
+(* Credit https://www.hackerrank.com/challenges/fp-hello-world -- "Hello World" *)
+
+(*	Compile with
+			ocamlbuild ocaml_exercise_1.byte ocaml_exercise_1.ml
+		at command line
+*)
+
+
+let () =
+    Printf.printf "Hello World\n";;
