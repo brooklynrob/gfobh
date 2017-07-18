1568f4836c74e08bf03eb2f13569cf1f254f4693
Rob Underwood <runderwood5@gmail.com>
2017-07-17 20:42:38 -0400

    Updates to Swift version of exercise 2

--- swift/swift_exercise_2.swift
+++ swift/swift_exercise_2.swift
@@ -1,11 +1,11 @@
 // Swift - Exercise 2
 // Inspired by https://www.hackerrank.com/challenges/fp-solve-me-first -- "Solve Me First FP"
 
-
 // Compile with following at command line
 // swiftc -o swift_exercise_2 swift_exercise_2.swift
 
 // Based in part on https://stackoverflow.com/questions/38167580/how-to-convert-swift-3-output-of-readline-to-integer
+
 let responsea = readLine()
 let responseb = readLine()
 
